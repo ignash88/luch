9fee0a05eabf10bf33ebbd99eb23f66caf2f393e
Roman Weiß <l_u_4@mail.ru>
2024-05-16 21:16:50 +0300

    dgsdg
    dhfdh

--- main.py
+++ main.py
@@ -10,14 +10,83 @@ class DataBase:
 
     def __create_table(self):
         sql = self.connect_db()
-        
+        sql["cursor"].execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id                INTEGER     PRIMARY KEY AUTOINCREMENT,
+                id_telegram       INTEGER     NOT NULL UNIQUE,
+                username          TEXT ,
+                first_name        TEXT,
+                last_name         TEXT, 
+                date_registration DATE,
+                access            BOOLEAN     DEFAULT 1                                                                                            
+            )
+        ''')
+        sql["cursor"].execute(''' 
+            CREATE TABLE IF NOT EXISTS messages(
+                id                INTEGER     PRIMARY KEY AUTOINCREMENT, 
+                id_user           INTEGER     NOT NULL, 
+                message_id        INTEGER     NOT NULL, 
+                message_text      TEXT        NOT NULL, 
+                date_send         DATE,
+                status            BOOLEAN     DEFAULT 0 CHECK(status IN (0,1)),
+                FOREIGN KEY (id_user)  REFERENCES users(id)                                                                                 
+            )
+        ''')
         self.close(sql["cursor"], sql["connect"])
 
     def connect_db(self):
         with sqlite3.connect(self.db_name) as connect:
             cursor = connect.cursor()
         return {"connect": connect, "cursor": cursor}
+    
+    def check_user(self,user_id):
+        sql = self.connect_db()
+        sql["cursor"].execute('''
+          SELECT * FROM users WHERE id_telegram =?
+        ''',(user_id,))
 
+        info_users = sql["cursor"].fetchone()
+        
+        self.close(sql["cursor"], sql["connect"])
+        if info_users is None:
+            return{
+                "status":False
+            }
+        return {
+            "status":True,
+            "info_user":info_users
+        }
+    def create_user(self,message:dict):
+        sql = self.connect_db()
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        sql["cursor"].execute('''
+            INSERT INTO users (
+              id_telegram,username,first_name,last_name,date_registration                
+            )VALUES (?,?,?,?,?)
+        ''',(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+            date 
+        ))
+        sql["connect"].commit()
+        self.close(sql["cursor"], sql["connect"])
+    def insert_message(self,message:dict):
+        sql = self.connect_db()
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        sql["cursor"].execute('''
+           INSERT INTO messager(        
+                id_user  ,        
+                message_id,        
+                message_text,      
+                date_send         
+            )   VALUES (?,?,?,?) 
+        ''',(
+             
+        ))
+        self.close(sql["cursor"], sql["connect"])
     def close(self, cursor, connect):
         cursor.close()
         connect.close()
@@ -34,26 +103,33 @@ class TelegramBot(DataBase):
 
         @self.bot.message_handler(commands=['start'])
         def start(message):
-            print(message)
+            text = ""
+            if self.check_user(message.from_user.id)["status"]:
+                text += "С возвращением"
+            else:
+                self.create_user(message)
+                text += f"Добро пожаловать, {message.from_user.first_name}!"
+            
+            # print(message)
             self.bot.send_message(
                 message.chat.id,
-                f"Добро пожаловать, {message.from_user.first_name}!"
+                text
             )
 
         @self.bot.message_handler(func=lambda message: True)
         def echo_all(message):
             self.bot.reply_to(
                 message,
-                "Не понимаю..."
+                "Сообщение отправлено админу!..."
             )
-            self.bot.delete_message(
-                chat_id=message.chat.id,
-                message_id=message.message_id
-            )
+            # self.bot.delete_message(
+            #     chat_id=message.chat.id,
+            #     message_id=message.message_id
+            # )
         self.bot.polling()
 
 
 TelegramBot(
     db_name="tg.db",
-    token=""
+    token="7082243137:AAFd1VJWs7Qb-DO0FteKIlmcE2C7YCeNZig"
 )
